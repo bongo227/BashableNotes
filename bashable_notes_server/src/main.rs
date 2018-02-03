bd516c3a273f549d3ec88e5efbc0e1c9a1c6dee5
Ben Sheffield <bshefb@gmail.com>
2018-02-03 01:44:16 +0000

    Created web socket server

--- bashable_notes_server/src/main.rs
+++ bashable_notes_server/src/main.rs
@@ -1,3 +1,30 @@
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+extern crate ws;
+extern crate time;
+
+mod server;
+
+use std::thread;
+
+use server::Server;
+use ws::listen;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let server_handle = thread::spawn(|| {
+        info!("websocket thread created");
+
+        let ws_address = "127.0.0.1:3012";
+        info!("Starting websocket on ws://{}", ws_address);
+        listen(ws_address, |out| Server {
+            out: out,
+            ping_timeout: None,
+            expire_timeout: None,
+        }).unwrap();
+    });
+
+    server_handle.join().unwrap();
 }
