cf399cb609434bcdad240133967539b4a38b554f
Ben Sheffield <bshefb@gmail.com>
2018-02-03 20:06:42 +0000

    Client requests documents over websocket

--- bashable_notes_client/src/filetree.js
+++ bashable_notes_client/src/filetree.js
@@ -1,36 +1,34 @@
 import React from "react";
 
-const SubMenu = ({children}) => (
-	<ul className="uk-nav-sub uk-nav-parent-icon" uk-nav="multiple: true">
-		{children}
-	</ul>
+const SubMenu = ({ children }) => (
+    <ul className="uk-nav-sub uk-nav-parent-icon" uk-nav="multiple: true">
+        {children}
+    </ul>
 );
 
-const File = ({name, path}) => (
-	<li>
-		<a href="#" onClick={() => {
-			console.log(`Clicked on ${name} with path ${path}`);
-		}}><span uk-icon="icon: file" className="uk-margin-small-right"></span>{name}</a>
-	</li>
+const File = ({ name, path, onClick }) => (
+    <li>
+        <a href="#" onClick={onClick}><span uk-icon="icon: file" className="uk-margin-small-right"></span>{name}</a>
+    </li>
 );
 
-const Folder = ({name, children}) => (
-	<li className="uk-parent">
-		<a href="#"><span uk-icon="icon: folder" className="uk-margin-small-right"></span>{name}</a>
-		<SubMenu>
-			{children}
-		</SubMenu>
-	</li>
+const Folder = ({ name, children }) => (
+    <li className="uk-parent">
+        <a href="#"><span uk-icon="icon: folder" className="uk-margin-small-right"></span>{name}</a>
+        <SubMenu>
+            {children}
+        </SubMenu>
+    </li>
 );
 
 export class FileTree extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {tree: []};
-        
+        this.state = { tree: [] };
+
         this.props.socket.addEventListener("message", (msg) => this.new_message(msg));
     }
-    
+
     new_message(msg) {
         try {
             let json_msg = JSON.parse(msg.data)
@@ -44,16 +42,35 @@ export class FileTree extends React.Component {
 
         }
     }
-    
+
+    request_file(path) {
+        try {
+            let req = {
+                "OpenFile": {
+                    "path": path,
+                }
+            };
+            this.props.socket.send(JSON.stringify(req));
+        } catch (e) {
+            console.warn("Failed to send OpenFile message: {}", e);
+        }
+    }
+
     render() {
         console.log("render");
         console.dir(this.state);
         let recurse_tree = (tree) => {
             return tree.map((item, index) => {
                 if ('File' in item) {
-                    return <File key={item.File.name+index} name={item.File.name} path={item.File.path} />;
+                    return <File 
+                        key={item.File.name + index} 
+                        name={item.File.name} 
+                        path={item.File.path} 
+                        onClick={() => this.request_file(item.File.path)} />;
                 } else {
-                    return <Folder key={item.Folder.name+index} name={item.Folder.name}>{recurse_tree(item.Folder.subtree)}</Folder>;
+                    return <Folder 
+                        key={item.Folder.name + index} 
+                        name={item.Folder.name}>{recurse_tree(item.Folder.subtree)}</Folder>;
                 }
             });
         };
--- bashable_notes_client/src/index.js
+++ bashable_notes_client/src/index.js
@@ -5,8 +5,31 @@ import { FileTree } from './filetree';
 import { Container, Spinner } from './uikit';
 import "./style.css";
 
-const Document = ({markdown, socket}) => {
-	return <div style={{marginLeft: 350}} dangerouslySetInnerHTML={{__html: markdown}}></div>
+class Document extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {markdown: "Open a file"};
+		this.props.socket.addEventListener("message", (msg) => this.new_message(msg));
+	}
+
+	new_message(msg) {
+        try {
+            let json_msg = JSON.parse(msg.data)
+            if ("Markdown" in json_msg) {
+                console.log("New Markdown message");
+                this.setState({
+                    markdown: json_msg.Markdown.markdown
+                });
+            }
+        } catch (e) {}
+    }
+
+	render() {
+		return <div 
+			style={{marginLeft: 350}} 
+			dangerouslySetInnerHTML={{__html: this.state.markdown}}>
+		</div>
+	} 
 }
 
 class App extends React.Component {
